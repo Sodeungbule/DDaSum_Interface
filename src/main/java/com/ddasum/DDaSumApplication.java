005f56e2e39976a82358fa2ec91ff9a6efc8c0fe
RosieOh <dhxogns920@gmail.com>
2025-07-15 00:24:00 +0900

    Build : Initial Commit

---
+++ src/main/java/com/ddasum/DDaSumApplication.java
@@ -0,0 +1,13 @@
+package com.ddasum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDaSumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDaSumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddasum/DDaSumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddasum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDaSumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
