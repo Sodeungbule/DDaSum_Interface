889ced520b07a76d21353a9b59f7fdca0495fd3b
RosieOh <dhxogns920@gmail.com>
2025-07-18 23:38:39 +0900

    FEAT : 빈집 생성 요청 DTO 추가

---
+++ src/main/java/com/ddasum/domain/vacanthouse/dto/VacantHouseCreateRequest.java
@@ -0,0 +1,13 @@
+package com.ddasum.domain.vacanthouse.dto;
+
+import lombok.Data;
+
+@Data
+public class VacantHouseCreateRequest {
+    private String address;
+    private String region;
+    private String description;
+    private int price;
+    private boolean available;
+    private String imageUrl;
+} 
