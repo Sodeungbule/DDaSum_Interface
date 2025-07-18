46ba1de7d631d43db3e29f1bc4948fb56e148dd7
RosieOh <dhxogns920@gmail.com>
2025-07-18 23:39:31 +0900

    FEAT : 빈집 비즈니스 레이어 인터페이스 추가

---
+++ src/main/java/com/ddasum/domain/vacanthouse/repository/VacantHouseRepository.java
@@ -0,0 +1,7 @@
+package com.ddasum.domain.vacanthouse.repository;
+
+import com.ddasum.domain.vacanthouse.entity.VacantHouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VacantHouseRepository extends JpaRepository<VacantHouse, Long> {
+} 
