c44874229b3dc6a1a9edb3d40c7c75967544300b
RosieOh <dhxogns920@gmail.com>
2025-07-18 23:56:01 +0900

    FEAT : 리뷰 업데이트 요청 DTO 추가

---
+++ src/main/java/com/ddasum/domain/review/dto/ReviewUpdateRequest.java
@@ -0,0 +1,9 @@
+package com.ddasum.domain.review.dto;
+import lombok.Data;
+
+@Data
+public class ReviewUpdateRequest {
+    private int rating;
+    private String content;
+    private String imageUrl;
+} 
